bdba5be7d5114150bcff538e2fb2f6f3eafbddb2
Tonlisboa <tonlisboa@gmail.com>
2021-08-20 22:44:53 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/olamundo/MainActivity.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.scl.sdm.olamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
